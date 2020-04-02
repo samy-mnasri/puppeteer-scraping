4a11704ac3efde43b75e3f2f889fa6b876986922
Samy M'Nasri <samy.mnasri@gmail.com>
2020-04-02 23:33:18 +0200

    starting to document

--- README.md
+++ README.md
@@ -1,3 +1,55 @@
 # puppeteer-scraping
 
-Blazing fast scraping with Puppeteer.
+Scrape anything with very few lines of code.
+
+`puppeteer-scraping` is a framework to help you save time scraping any website with Puppeteer.
+
+It uses `puppeteer-extra-plugin-stealth` under the hood.
+
+## Motivation
+
+- Scraping websites is often about following the same steps
+- We should code only what's unique to the scraped website: the scraping logic, the paths taken and the data extracted
+- Puppeteer is the way to go for JavaScript rendered websites but it could be easier to use
+
+## Installation
+
+Using npm:
+
+```bash
+npm install puppeteer-scraping
+```
+
+Using yarn:
+
+```bash
+yarn add puppeteer-scraping
+```
+
+## Usage
+
+```javascript
+const scraping = require('puppeteer-scraping')
+const puppeteer = require('puppeteer')
+
+module.exports = async (req, res) => {  
+  const { items } = await scraping({
+    puppeteer,
+    options: { headless: true },
+    method: {
+      startPage: 'https://example.com',
+      goToPages: {
+        '//a[@class="item"]/@href': {
+          items: {
+            products: {
+              productTitle: { path: '//h1' }
+            },
+          }
+        }
+      }
+    }
+  })
+  
+  res.json(items)
+}
+```
--- index.js
+++ index.js
@@ -1,67 +1,77 @@
-module.exports = async ({ options, puppeteer, method }) => {
+const { addExtra } = require('puppeteer-extra')
+const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+
+module.exports = async ({ options, puppeteer, method, onError }) => {
   const context = { items: [] }
 
-  const browser = await puppeteer.launch(options)
+  const puppeteerExtra = addExtra(puppeteer)
+  const browser = await puppeteerExtra.launch(options)
+  StealthPlugin().onBrowser(browser)
   context.page = await browser.newPage()
+  StealthPlugin().onPageCreated(context.page)
   let nextPage
 
-  do {
-    await context.page.goto(nextPage || method.startPage)
-    const urls = []
-    const urlElements = await context.page.$x(method.goToPages.path)
+  try {
+    do {
+      await context.page.goto(nextPage || method.startPage)
+      const urls = []
+      const urlElements = await context.page.$x(method.goToPages.path)
 
-    for (const urlElement of urlElements) {
-      urls.push(await (await urlElement
-        .getProperty('textContent'))
-        .jsonValue())
-    }
+      for (const urlElement of urlElements) {
+        urls.push(await (await urlElement
+          .getProperty('textContent'))
+          .jsonValue())
+      }
 
-    for (const url of urls) {
-      if (!method.goToPages.pageAlreadyScraped || !await method.goToPages.pageAlreadyScraped(url, context)) {
-        await context.page.goto(url)
+      for (const url of urls) {
+        if (!method.goToPages.pageAlreadyScraped || !await method.goToPages.pageAlreadyScraped(url, context)) {
+          await context.page.goto(url)
 
-        for (const itemMethod of method.goToPages.items) {
-          let item = {}
+          for (const itemMethod of method.goToPages.items) {
+            let item = {}
 
-          for (const property in itemMethod.data) {
-            if (itemMethod.data[property].value) {
-              item[property] = await itemMethod.data[property].value(context)
-            } else {
-              const values = []
-
-              const elements = await context.page
-                .$x(itemMethod.data[property].path)
-
-              for (const element of elements) {
-                values.push(
-                  await (await element
-                    .getProperty('textContent'))
-                    .jsonValue()
-                )
-              }
-
-              if (itemMethod.data[property].getFirst) {
-                item[property] = values[0]
+            for (const property in itemMethod.data) {
+              if (itemMethod.data[property].value) {
+                item[property] = await itemMethod.data[property].value(context)
               } else {
-                item[property] = values
+                const values = []
+
+                const elements = await context.page
+                  .$x(itemMethod.data[property].path)
+
+                for (const element of elements) {
+                  values.push(
+                    await (await element
+                      .getProperty('textContent'))
+                      .jsonValue()
+                  )
+                }
+
+                if (itemMethod.data[property].getFirst) {
+                  item[property] = values[0]
+                } else {
+                  item[property] = values
+                }
               }
             }
-          }
 
-          item = (await itemMethod.processItem(item, context)) || item
+            item = (await itemMethod.processItem(item, context)) || item
 
-          if (context.items[itemMethod.type]) {
-            context.items[itemMethod.type].push(item)
-          } else {
-            context.items[itemMethod.type] = [item]
+            if (context.items[itemMethod.type]) {
+              context.items[itemMethod.type].push(item)
+            } else {
+              context.items[itemMethod.type] = [item]
+            }
           }
         }
       }
-    }
 
-    nextPage = await method.nextPage(context)
-  } while (nextPage)
+      nextPage = await method.nextPage(context)
+    } while (nextPage)
 
-  await browser.close()
-  return context
+    await browser.close()
+    return context
+  } catch (error) {
+    onError(error, context)
+  }
 }
